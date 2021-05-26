0c84dabdf83f817c69de2dc34987e253a27faf2f
Ashley D'Allessandro <ashley.dallessandro@gmail.com>
2021-05-26 14:46:01 -0700

    just trying to make this work

--- src/components/StoryA/Point1/Point1A/Point1A.js
+++ src/components/StoryA/Point1/Point1A/Point1A.js
@@ -1,7 +1,9 @@
 import React, { useState }from 'react';
 import { Router, Link } from '@reach/router';
 import Point1A1 from './Point1A1';
+import A_Start from '../../../../images/A_Start.jpeg';
 import Point1B from '../Point1B/Point1B';
+
 
 const Point1A = () => {
     const [selected, setSelected] = useState(false);
@@ -9,14 +11,15 @@ const Point1A = () => {
         <div>
             {
                 !selected ? 
-                <>
+                <div className = "img" style = {
+                    {backgroundImage:`url(${A_Start})`}}>
                 <p>What i loved to do most was</p>
                 <Link onClick = {() => setSelected(true)} to="1a1">Continue to a</Link>
                 <br />
                 <br />
                 <Link onClick = {() => setSelected(true)} to="1b">got to B</Link>
 
-                </>
+                </div>
                 :
                 ""
             }
